4080a0aa2408a1b34930de5bee121449b29eda5f
Thejus Thomson <thomson.th@northeastern.edu>
2025-05-08 02:27:25 -0400

    5/7 chnages

---
+++ dsa/3sum.py
@@ -0,0 +1,22 @@
+nums = [0,1,1]
+answer = []
+le = len(nums)
+nums.sort()
+for i in range(le):
+    if nums[i] > 0:
+        break
+    elif i > 0 and nums[i-1] == nums[i]:
+        continue
+    l, r = i+1, le-1
+    while l < r:
+        sum  = nums[i] + nums[l] + nums[r]
+        if sum > 0:
+            r -= 1
+        elif sum < 0:
+            l += 1
+        else:
+            answer.append([nums[i],nums[l],nums[r]])
+            l +=1
+            while l < r and nums[l] == nums[l-1]:
+                l += 1
+print(answer)
